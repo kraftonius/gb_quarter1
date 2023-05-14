6ae4d10e2b6decba05fcfc022923af40735afe9b
kraftonius <avd@yandex.ru>
2023-05-14 17:15:39 +0300

    Fix Program.cs, README.MD, task_diagram.drawio.svg

--- Program.cs
+++ Program.cs
@@ -6,28 +6,92 @@
 // При решении не рекомендуется пользоваться коллекциями, 
 // лучше обойтись исключительно массивами.
 
-void printStringArray(string[] arr)
-{
-    Console.Write("[");
-    for (int i = 0; i < arr.Length - 1; i++)
-    {
-        if (arr[i] == string.Empty)
-        {
+// Примеры:
+// [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+// [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+// [“Russia”, “Denmark”, “Kazan”] → []
 
-        }
-        else
+
+void printStringArray(string[] arr, bool skipEmpty = false)
+// Функция выводить массив. Если skipEmpty = true - то пустые значения не выводятся
+{
+    bool[] commaArr = new bool[arr.Length];
+    if (skipEmpty)
+    {
+        for (int i = 0; i < arr.Length - 1; i++)
         {
-        Console.Write(arr[i]);
-        Console.Write(", ");
+            if (arr[i] != string.Empty) commaArr[i] = true;
         }
-       
+        int skipCommaIndex = arr.Length - 1;
+        for (int j = arr.Length - 1; j >= 0; j--)
+        {
+            if (arr[j] != string.Empty) skipCommaIndex = j;
+            j = -1;
+        }
+        commaArr[skipCommaIndex] = false;
     }
-    Console.Write(arr[arr.Length - 1]);
+    Console.Write("[");
+    for (int k = 0; k < arr.Length; k++)
+    {
+        Console.Write(arr[k]);
+        if (skipEmpty && commaArr[k]) Console.Write(", ");
+        else if (!skipEmpty && k != arr.Length - 1) Console.Write(", ");
+    }
+
     Console.Write("]");
 }
 
-string[] array = new string[5] {"Elephant", "Koala", "", "E", "Jaguar"};
+string[] CutStringsInArray(string[] arr, int maxLength)
+{
+    if (maxLength  < 0) maxLength  = 0;
+    string[] newArr = new string[arr.Length];
+    int stringLength = 0;
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length == 0)
+        {
+            stringLength = 0;
+        }
+        else if (arr[i].Length >= maxLength)
+        {
+            stringLength = rnd.Next(0, maxLength + 1);
+        }
+        else
+        {
+            stringLength = rnd.Next(0, arr[i].Length + 1);
+        }
+
+        newArr[i] = string.Empty;
+        for (int k = 0; k < stringLength; k++)
+        {
+            newArr[i] += arr[i][k];
+        }
+    }
+    return newArr;
+}
+
+string[] array = new string[5] { "Elephant", "Koala", "", "E", "Jaguar" };
+int maxStringLength = 3;
+string[] newArray = CutStringsInArray(array, maxStringLength);
 
 printStringArray(array);
 Console.Write(" -> ");
-printStringArray(array);
+printStringArray(newArray, skipEmpty: true);
+
+
+string[] array1 = new string[5] { "A1", "A2", "", "A4", "" };
+string[] array2 = new string[5] { "A1", "A2", "", "", "A5" };
+string[] array3 = new string[5] { "A1", "", "", "", "A5" };
+string[] array4 = new string[5] { "A1", "", "", "", "" };
+string[] array5 = new string[5] { "", "", "A3", "", "" };
+Console.WriteLine();
+printStringArray(array1, skipEmpty: true);
+Console.WriteLine();
+printStringArray(array2, skipEmpty: true);
+Console.WriteLine();
+printStringArray(array3, skipEmpty: true);
+Console.WriteLine();
+printStringArray(array4, skipEmpty: true);
+Console.WriteLine();
+printStringArray(array5, skipEmpty: true);
